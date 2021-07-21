e66991cd4a79f3d7990ae19caf66e273cba8eaa4
leiliang22 <leiliang835@163.com>
2021-07-21 09:12:53 +0800

    20210721

--- tinymemcached/memcached.c
+++ tinymemcached/memcached.c
@@ -22,6 +22,7 @@
 #include <assert.h>
 #include <sysexits.h>
 #include <stddef.h>
+#include <stdbool.h>
 
 /*
  * forward declarations
@@ -32,6 +33,51 @@ static ssize_t tcp_read(conn *arg, void *buf, size_t count);
 static ssize_t tcp_sendmsg(conn *arg, struct msghdr *msg, int flags);
 static ssize_t tcp_write(conn *arg, void *buf, size_t count);
 
+static int stop_main_loop = NOT_STOP;
+static conn *listen_conn = NULL;
+static int max_fds;
+struct event_base *main_base;
+
+conn *conn_new(const int sfd, enum conn_states init_state,
+                const int event_flags,
+                const int read_buffer_size, enum network_transport transport,
+                struct event_base *base, void *ssl) {
+    return NULL;
+}
+
+static int server_socket(const char *interface,
+                         int port,
+                         enum network_transport transport,
+                         FILE *portnumber_file, bool ssl_enabled) {
+    int sfd = socket(AF_INET,SOCK_STREAM,0);
+    int flags;
+
+    if ((flags = fcntl(sfd, F_GETFL, 0)) < 0 ||
+        fcntl(sfd, F_SETFL, flags | O_NONBLOCK) < 0) {
+        perror("setting O_NONBLOCK");
+        close(sfd);
+        return -1;
+    }
+
+    struct sockaddr_in local_addr = {0};
+	local_addr.sin_family = AF_INET;//使用IPv4
+	local_addr.sin_port = htons(port);//port是端口号，网络传输使用大端字节序，使用htons()进行转换。端口是int类型，范围0~65535
+	local_addr.sin_addr.s_addr = INADDR_ANY;
+
+    bind(sfd,(struct sockaddr *)&local_addr,sizeof(local_addr));
+
+    conn *listen_conn_add;
+    listen_conn_add = conn_new(sfd, conn_listening, EV_READ | EV_PERSIST, 1,
+                             transport, main_base, NULL);
+    listen_conn_add->next = listen_conn;
+    listen_conn = listen_conn_add;
+
+    return 1;
+}
+
+static int server_sockets(int port, enum network_transport transport, FILE *portnumber_file) {
+    return server_socket(NULL, port, transport, NULL, false);
+}
 
 /* Default methods to read from/ write to a socket */
 ssize_t tcp_read(conn *c, void *buf, size_t count) {
@@ -50,8 +96,6 @@ ssize_t tcp_write(conn *c, void *buf, size_t count) {
 }
 
 
-
-struct event_base *main_base;
 
 int main() {
     struct event_config *ev_config;
--- tinymemcached/memcached.h
+++ tinymemcached/memcached.h
@@ -10,6 +10,36 @@
 #include <grp.h>
 #include <signal.h>
 
+
+enum conn_states {
+    conn_listening,  /**< the socket which listens for connections */
+    conn_new_cmd,    /**< Prepare connection for next command */
+    conn_waiting,    /**< waiting for a readable socket */
+    conn_read,       /**< reading in a command line */
+    conn_parse_cmd,  /**< try to parse a command from the input buffer */
+    conn_write,      /**< writing out a simple response */
+    conn_nread,      /**< reading in a fixed number of bytes */
+    conn_swallow,    /**< swallowing unnecessary bytes w/o storing */
+    conn_closing,    /**< closing this connection */
+    conn_mwrite,     /**< writing out many items sequentially */
+    conn_closed,     /**< connection is closed */
+    conn_watch,      /**< held by the logger thread as a watcher */
+    conn_io_queue,   /**< wait on async. process to get response object */
+    conn_max_state   /**< Max state value (used for assertion) */
+};
+
+enum network_transport {
+    local_transport, /* Unix sockets*/
+    tcp_transport,
+    udp_transport
+};
+
+enum stop_reasons {
+    NOT_STOP,
+    GRACE_STOP,
+    EXIT_NORMALLY
+};
+
 typedef struct conn conn;
 
 /**
@@ -17,4 +47,5 @@ typedef struct conn conn;
  */
 struct conn {
     int    sfd;
+    conn   *next;
 };
